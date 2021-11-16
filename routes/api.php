828bf15afaa812ac5eac8a8fde6e2b7247e0460e
zanifnissa01 <zanifchoirunissa@gmail.com>
2021-11-16 12:05:16 +0700

    praktikum1

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register',[AuthController::classs,'register']);
+Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);

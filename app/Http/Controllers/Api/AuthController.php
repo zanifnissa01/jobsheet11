5d6d5a6da7b29ac18c1dc5814aa4c8463514242b
zanifnissa01 <zanifchoirunissa@gmail.com>
2021-11-17 13:54:16 +0700

    praktikum1

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Traits\ApiResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
